490aa8cb0e0ebbbb2fc2128e5e67a482a8e47c0a
Harold <haroldmo23@gmail.com>
2024-08-03 16:53:17 -0700

    changed to hashrouter for redirecting

--- portfolio/src/components/navbar/navbar.js
+++ portfolio/src/components/navbar/navbar.js
@@ -26,11 +26,11 @@ export default function ButtonAppBar() {
                 </Box>
 
                 <Box sx={{ flex: 1, display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
-                    <Button color="inherit" href='portfolio/#/about_me' sx={{fontSize: '20px'}}>About Me</Button>
-                    <Button color="inherit" href='portfolio/#/work_experience' sx={{fontSize: '20px'}}>Work Experience</Button>
-                    <Button color="inherit" href='portfolio/#/education' sx={{fontSize: '20px'}}>Education</Button>
-                    <Button color="inherit" href='portfolio/#/projects' sx={{fontSize: '20px'}}>Projects</Button>
-                    <Button color="inherit" href='portfolio/#/contact' sx={{fontSize: '20px'}}>Contact</Button>
+                    <Button color="inherit" href='#/about_me' sx={{fontSize: '20px'}}>About Me</Button>
+                    <Button color="inherit" href='#/work_experience' sx={{fontSize: '20px'}}>Work Experience</Button>
+                    <Button color="inherit" href='#/education' sx={{fontSize: '20px'}}>Education</Button>
+                    <Button color="inherit" href='#/projects' sx={{fontSize: '20px'}}>Projects</Button>
+                    <Button color="inherit" href='#/contact' sx={{fontSize: '20px'}}>Contact</Button>
                 </Box>
             </Box>
         </Toolbar>
